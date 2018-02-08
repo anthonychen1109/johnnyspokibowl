2dfbeb84cc285cb5220a61f80f51f1f48a032118
ac065455 <chen.anthony.1109@gmail.com>
2018-02-08 10:30:07 -0500

    moved button outside of logo

--- src/components/header.js
+++ src/components/header.js
@@ -6,10 +6,14 @@ import Logo from '../static/images/logo.png';
 const Header = () => {
   return (
     <div className="header">
+      <div className="header-wrapper">
       <div className="header-logo">
         <img src={Logo} alt="logo"/>
-        <span class="glf-button" data-glf-cuid="b6cb6133-f364-495b-ad68-298f3197fae3" data-glf-ruid="abdffdf9-c8cb-474b-9e45-24b0911fdbcf" > See MENU & Order</span>
       </div>
+      <div>
+        <span className="glf-button" data-glf-cuid="b6cb6133-f364-495b-ad68-298f3197fae3" data-glf-ruid="abdffdf9-c8cb-474b-9e45-24b0911fdbcf" > See Menu & Order</span>
+      </div>
+    </div>
     </div>
   )
 }
